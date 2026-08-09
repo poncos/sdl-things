2450cf9b3cf50deb4dbd67f1b408588b81e70d8a
Esteban Collado <esteban.collado@pm.me>
2026-08-09 02:25:15 +0100

    empty sdl template

---
+++ template/main.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include "SDLApp.hpp"
+
+
+int main() {
+    SDLApp theApp;
+
+    if (theApp.init() != 0) {
+        return 1;
+    }
+    
+    theApp.run();
+
+    return 0;
+}
